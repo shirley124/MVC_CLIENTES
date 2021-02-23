4a6336a93ada1cda57ea4d56643b28fb480c5930
Lourdes <Lourdes@LAPTOP-7UTQB1N7>
2021-02-23 19:36:07 +0100

    Agregar archivos de proyecto.

---
+++ MVC_CLIENTES/Models/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_CLIENTES.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Direccion { get; set; }
+        public  string Telefono { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de alta")]
+        public DateTime FechaAlta { get; set; }
+    }
+}
